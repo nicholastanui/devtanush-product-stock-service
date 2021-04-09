beafe0e96b08c84840173ecb71d6bc83c2685b5f
ntanui <ntanui@safaricom.co.ke>
2021-04-09 16:57:20 +0300

    init setup

---
+++ src/test/java/com/devtanush/microservices/productstockservice/devtanushproductstockservice/DevtanushProductStockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtanush.microservices.productstockservice.devtanushproductstockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtanushProductStockServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
